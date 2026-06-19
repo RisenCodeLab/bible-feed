5fa54df64947015d2955727705096f23f87d872d
Andrew <dev@risencode.com>
2026-06-19 14:27:25 +0100

    fix lint

--- lib/view/feeds.dart
+++ lib/view/feeds.dart
@@ -7,17 +7,20 @@ import 'feed.dart';
 class Feeds extends StatelessWidget {
   // Columns and Rows work better than a GridView
   @override
-  build(context) => Column(
+  build(context) {
+    var feedsManager = sl<FeedsManager>();
+    return Column(
     children: [
       for (int index in [0, 2, 4, 6, 8])
         Expanded(
           child: Row(
             children: [
-              Expanded(child: Feed(sl<FeedsManager>().feeds[index])),
-              Expanded(child: Feed(sl<FeedsManager>().feeds[index + 1])),
+              Expanded(child: Feed(feedsManager.feeds[index])),
+              Expanded(child: Feed(feedsManager.feeds[index + 1])),
             ],
           ),
         ),
     ],
   );
+  }
 }
